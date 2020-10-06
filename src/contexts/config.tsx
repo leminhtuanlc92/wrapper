1da287c921b0b8a5c981d3c52bb9d99294022393
LeMinhTuan <>
2020-10-06 09:54:10 +0700

    factory address change

--- src/contexts/config.tsx
+++ src/contexts/config.tsx
@@ -1,6 +1,6 @@
 const node = "https://api.shasta.trongrid.io";
 const contract = {
     getAddress: "THE9WMdxzgM5UNgW3ZN3pFicTJfjTyszYK",
-    factoryAddress: "TYqjU4yqD8iDMBfqejCWBKsUqwFEDxZbWT"
+    factoryAddress: "TG7nvFHaaC7z3u34g2CNiJ6dAzreeZVgSb"
 };
 export { node, contract };
